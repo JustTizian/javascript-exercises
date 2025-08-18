e9a9e2223e998e130755ab045bf69b14c7de6ed5
tihi <77891364+JustTizian@users.noreply.github.com>
2025-08-18 19:09:09 +0200

    Solve09_sumall

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let sum = 0;
+    if(min < 0 || max < 0) return "ERROR"
+    if(Math.floor(min) !== min ||Math.floor(max) !== max ) return "ERROR"
+    if(typeof min !== "number" || typeof max !== "number") return "ERROR"
+    
+    if(min > max){
+        [min, max] = [max, min]
+    }
+    for(let i = min; i <= max; i++){
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
