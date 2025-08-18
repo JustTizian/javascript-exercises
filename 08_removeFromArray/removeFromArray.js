dbc6db2c0414dd6e3cf162b62611c87146ee5573
tihi <77891364+JustTizian@users.noreply.github.com>
2025-08-18 18:53:35 +0200

    Solve08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    const toRemove = Array.from(args)
+
+    toRemove.forEach(condition => {
+        while(arr.some(value => value === condition)){
+            const indexToBeRemoved = arr.findIndex((value) => value === condition)
+            arr.splice(indexToBeRemoved, 1)
+        }
+    })
+
+    return arr;
 };
 
 // Do not edit below this line
