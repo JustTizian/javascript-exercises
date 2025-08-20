728845bd95c4a4df6fc1b9cafd891a1628eb7e4c
tihi <77891364+JustTizian@users.noreply.github.com>
2025-08-20 11:31:09 +0200

    Solve 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    return arr.sort((p1, p2) => {
+        const p1age = p1.yearOfDeath
+            ? p1.yearOfDeath - p1.yearOfBirth
+            : new Date().getFullYear() - p1.yearOfBirth
 
+        const p2age = p2.yearOfDeath
+            ? p2.yearOfDeath - p2.yearOfBirth
+            : new Date().getFullYear() - p2.yearOfBirth
+
+        return p2age - p1age
+    })[0]
 };
 
+/*
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+*/
 // Do not edit below this line
 module.exports = findTheOldest;
