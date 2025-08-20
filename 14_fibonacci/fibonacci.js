00aaa37d8a54cee42cde8de8f8d60e31724f0399
tihi <77891364+JustTizian@users.noreply.github.com>
2025-08-20 11:18:05 +0200

    Solve 14_Fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (typeof num !== Number) {
+        count = parseInt(num)
+    } else {
+        count = num
+    }
 
+    if (count < 0) return "OOPS"
+
+
+    const fib = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[num];
 };
 
 // Do not edit below this line
