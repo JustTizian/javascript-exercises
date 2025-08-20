a2db04cd7a436540d8a6ffdcecaaf1bdc161477c
tihi <77891364+JustTizian@users.noreply.github.com>
2025-08-20 10:55:55 +0200

    Solve 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanString = string
+    .replaceAll(' ', '')
+    .replaceAll('!', '')
+    .replaceAll('.', '')
+    .replaceAll('?', '')
+    .replaceAll(',', '')
+    .toLowerCase()
+    
+    return cleanString.split('').reverse().join('') == cleanString
+    
 };
 
 // Do not edit below this line
